32411234cbc05bd5324f644f8ee777cc44a85821
HasancanCakicioglu <hckecommerce@gmail.com>
2023-12-03 21:44:13 +0300

    commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,12 @@
 import os
 import sys
 
+# Test dosyasının bulunduğu dizinden bir adım geri gidin
+current_dir = os.path.dirname(__file__)
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
 
+# sys.path'e ekleyin
+sys.path.append(project_root)
 
 from src.main import print_hi
 
